d92df438570e21affe431aa769730ab52b751cc0
badr ghanbi <bghanbi50@gmail.com>
2022-04-22 09:55:38 +0000

    add action btns

--- app/app.js
+++ app/app.js
@@ -2,28 +2,43 @@ const addTodo=document.querySelector('.hero__newTodo');
 const todosList=document.querySelector('.todos');
 const togglers=document.querySelectorAll('.nav__toggler');
 const  numberOfItems=document.querySelector('.item-left-number');
+const active=document.querySelector('.active');
+const all=active.previousElementSibling;
+const completed=active.nextElementSibling;
+const clearCompleted=completed.nextElementSibling;
+
 // add new task when press the enter keyword
 addTodo.addEventListener('keydown',(e)=>{
     if (e.key==='Enter' && addTodo.firstElementChild.value){
+        // add the circle
+        let circle=document.createElement('span');
+        circle.innerHTML='<span></span>';
+        circle.classList.add('circle');
+        // add the check
+        let check=document.createElement('span');
+        check.innerHTML='<span></span>';
+        check.classList.add('check');
+        // add the li 
      let task=document.createElement('li');
      task.innerHTML= `<p>${addTodo.firstElementChild.value}</p>`;
       task.classList.add('col');
-      task.classList.add('circleBefore');
+      task.firstChild.classList.add('fixPosition');
       task.classList.add('col-added');
       todosList.append(task);
+      task.append(circle);
+      task.append(check)
     //  increase number of item added
     numberOfItems.textContent++;
     }
 });
 
-// Hover effect on todos Container
- todosList.addEventListener('mouseover',(e)=>e.target.classList.add('showCheck'));
- todosList.addEventListener('mouseout',(e)=>e.target.classList.remove('showCheck'));
+
 
 // Update the value of input to default when adding new task
-addTodo.addEventListener('keyup',(e)=>{
-    if (e.key==='Enter') addTodo.firstElementChild.value='';
-})
+
+addTodo.addEventListener('keyup',(e)=>(e.key==='Enter')?addTodo.firstElementChild.value='':'');
+
+
 
 // change the toggler
  togglers.forEach(toggler=>toggler.addEventListener('click',(e)=>{
@@ -36,8 +51,60 @@ addTodo.addEventListener('keyup',(e)=>{
          e.target.previousElementSibling.classList.toggle('hide') 
      }
  }))
-//      console.log(getComputedStyle(e.target,'::after').getPropertyValue('content'));
-  todosList.addEventListener('click',(e)=>{
-    // console.log(getComputedStyle(e.target,'::after').getPropertyValue('content'))
-    console.log(e.target.window.offsetX)
-  })
+
+
+// click on circle and change bg
+todosList.addEventListener('click',(e)=>{
+    if (e.target.classList.contains('circle')){
+        e.target.classList.toggle('bg');
+        e.target.previousElementSibling.classList.toggle('taskRemove');
+        // updateNumberOfItems();
+        e.target.classList.contains('bg')?numberOfItems.textContent--:numberOfItems.textContent++;
+    };
+})
+
+todosList.addEventListener('click',(e)=>{
+   if (e.target.classList.contains('check')){
+    e.target.parentElement.remove();
+    (e.target.previousElementSibling.classList.contains('bg'))?'':numberOfItems.textContent--;
+   }
+})
+
+// actions btn 
+
+function removeHideFromElement(){
+    todosList.querySelectorAll('li').forEach(element=>element.classList.remove('hide'));
+}
+
+// all
+all.addEventListener('click',()=> removeHideFromElement());
+
+
+// active
+active.addEventListener('click',()=>{
+ const circleChecked= todosList.querySelectorAll('.bg');
+ removeHideFromElement();
+ circleChecked.forEach(circle=>circle.parentElement.classList.add('hide'));
+});
+
+
+// completed
+completed.addEventListener('click',()=>{
+    todosList.querySelectorAll('li').forEach(element=>{
+    if (element.querySelector('.circle').classList.contains('bg')){
+        element.classList.remove('hide');
+    }
+    else{
+        element.classList.add('hide');
+    }
+    });
+      })
+
+// clear completed
+clearCompleted.addEventListener('click',()=>{
+    todosList.querySelectorAll('li').forEach(element=>{
+       if (element.querySelector('.circle').classList.contains('bg')){
+           element.remove();
+       }
+    })
+})
